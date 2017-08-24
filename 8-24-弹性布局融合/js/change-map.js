c86a4e81dfd52340a0291286e5534d3f17ed34ca
FishC-BR <13618482846@163.com>
2017-08-24 14:27:39 +0800

    完成-echart注入
    实现流动布局

---
+++ 8-24-弹性布局融合/js/change-map.js
@@ -0,0 +1,80 @@
+$(function(){
+	changeMap();
+})
+
+
+function changeMap(){
+	var myChart= echarts.init(document.getElementById('map'));
+	option = {
+	    tooltip: {
+	        trigger: 'item',
+	        formatter: '{b}'
+	    },
+	    series: [
+	        {
+	            name: '中国',
+	            type: 'map',
+	            mapType: 'china',
+	            selectedMode : 'single',
+	            label: {
+	                normal: {
+	                    show: true
+	                },
+	                emphasis: {
+	                    show: true
+	                }
+	            },
+	             data:[
+                
+            ]
+	        }
+	    ]  
+	};
+	var Province = "";
+
+	myChart.on('click', function (params){
+		var myChart= echarts.init(document.getElementById('map'));
+		Province = params.name;
+		option = {
+		    tooltip: {
+		        trigger: 'item',
+		        formatter: '{b}'
+		    },
+		    series: [
+		        {
+		            name: '中国',
+		            type: 'map',
+		            mapType: Province,
+		            selectedMode : 'single',
+		            label: {
+		                normal: {
+		                    show: true
+		                },
+		                emphasis: {
+		                    show: true
+		                }
+		            },
+		             data:[
+	                
+	            ]
+		        }
+		    ]
+		};
+
+		myChart.on('click', function (params){
+			changeMap();
+		});
+
+		//绘制省级
+		myChart.setOption(option);
+ 		window.addEventListener("resize",function(){
+       	 	myChart.resize();
+   		});
+	});
+
+	// 绘制全国
+	myChart.setOption(option);
+ 	window.addEventListener("resize",function(){
+        myChart.resize();
+   });
+}
