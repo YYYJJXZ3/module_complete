ecc586de2fdb23e63cc00671bbda8b70baba9466
FishC-BR <13618482846@163.com>
2017-08-28 16:18:50 +0800

    完成实时颜色修改
    每次刷新都会重绘

--- 8-28-测试部分选择/js/onload.js
+++ 8-28-测试部分选择/js/onload.js
@@ -13,29 +13,17 @@ function append() {
           `;
         })
         $jsontip.html(strHtml);//显示处理后的数据
+
+
+        // 前十个tr中，第一个td中的b，设置背景样式。
+        $.each($('tr').slice(1,11),function(i,item){
+            $(item).children('td').eq(0).children('b').css("background-color","#3590de");
+        });
+
+        $.each($('tr').slice(1,7),function(i,item){
+            $(item).children('td').eq(1).css("color","#0002c0");
+        });
     })
 }
 
-// $(document).ready(function(){
-//     // 前十个tr中，第一个td中的b，设置背景样式。
-//     $.each($('tr').slice(1,11),function(i,item){
-//         $(item).children('td').eq(0).children('b').css("background-color","#3590de");
-//     });
-//
-//     $.each($('tr').slice(1,7),function(i,item){
-//         $(item).children('td').eq(1).css("color","#0002c0");
-//     });
-//
-//
-//
-// });
-$(function(){
-    // 前十个tr中，第一个td中的b，设置背景样式。
-    $.each($('tr').slice(1,11),function(i,item){
-        $(item).children('td').eq(0).children('b').css("background-color","#3590de");
-    });
 
-    $.each($('tr').slice(1,7),function(i,item){
-        $(item).children('td').eq(1).css("color","#0002c0");
-    });
-});
