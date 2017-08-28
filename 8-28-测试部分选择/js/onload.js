c556f9b455a3b848c1ad19e4dd4de4a658441d2a
FishC-BR <13618482846@163.com>
2017-08-28 14:28:34 +0800

    实现动态json样式设置
    核心：
      $.each($('tr').slice(1,11),function(i,item){
    
    $(item).children('td').eq(0).children('b').css("background-color","#3590
    de");
        });
    
        $.each($('tr').slice(1,7),function(i,item){
            $(item).children('td').eq(1).css("color","#0002c0");
        });
    
    注意：slice切割-》1即从第二位开始取，index从0开始，而0是不需要的默认表头项

---
+++ 8-28-测试部分选择/js/onload.js
@@ -0,0 +1,31 @@
+function append() {
+    $.getJSON("js/userinfo.json", function (data) {
+        var $jsontip = $("#jsonTip");
+        var strHtml = "";//存储数据的变量
+        $jsontip.empty();//清空内容
+        $.each(data, function (infoIndex, info) {
+            strHtml += `
+            <tr>
+              <td><b class="roleNum">${info["num"]}</b></td>
+              <td>${info["size"]}</td>
+              <td>${info["data"]}</td>
+                      </tr>
+          `;
+        })
+        $jsontip.html(strHtml);//显示处理后的数据
+    })
+}
+
+$(document).ready(function(){
+    // 前十个tr中，第一个td中的b，设置背景样式。
+    $.each($('tr').slice(1,11),function(i,item){
+        $(item).children('td').eq(0).children('b').css("background-color","#3590de");
+    });
+
+    $.each($('tr').slice(1,7),function(i,item){
+        $(item).children('td').eq(1).css("color","#0002c0");
+    });
+
+
+
+});
